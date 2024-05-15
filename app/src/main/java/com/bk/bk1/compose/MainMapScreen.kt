e8951225d6f72f0cbf3c886db08eb6420beecee5
Ondřej Dresler <o-dresler@seznam.cz>
2024-05-15 16:26:57 +0200

    Move map markers to file.

--- app/src/main/java/com/bk/bk1/compose/MainMapScreen.kt
+++ app/src/main/java/com/bk/bk1/compose/MainMapScreen.kt
@@ -3,10 +3,7 @@
 package com.bk.bk1.compose
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.graphics.Bitmap
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -33,7 +30,6 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.outlined.Warning
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -67,9 +63,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.BlendModeColorFilterCompat
-import androidx.core.graphics.BlendModeCompat
 import androidx.navigation.NavController
 import com.bk.bk1.R
 import com.bk.bk1.enums.SensorConnectionStatus
@@ -89,18 +82,13 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.MultiplePermissionsState
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.maps.android.compose.CameraMoveStartedReason
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
-import com.google.maps.android.compose.MarkerInfoWindow
-import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
-import com.google.maps.android.compose.rememberMarkerState
 import com.google.maps.android.ui.IconGenerator
 import com.leinardi.android.speeddial.compose.FabWithLabel
 import com.leinardi.android.speeddial.compose.SpeedDial
@@ -418,73 +406,7 @@ fun SpeedDialButtonMenu(
     }
 }
 
-@SuppressLint("SetTextI18n")
-@Composable
-fun TrackMarker(
-    record: ComfortIndexRecord,
-    navController: NavController,
-    markerView: View,
-    markerIconView: ImageView,
-    markerLabel: TextView,
-    iconGenerator: IconGenerator
-) {
-    markerIconView.setImageResource(R.drawable.bicycle_pin_48)
-    markerLabel.text = "${stringResource(R.string.label_track_x)} ${record.trackRecordId}"
 
-    iconGenerator.setBackground(null)
-    iconGenerator.setContentView(markerView)
-    MarkerInfoWindow(
-        state = rememberMarkerState(
-            position = LatLng(record.latitude, record.longitude)
-        ),
-        icon = BitmapDescriptorFactory.fromBitmap(iconGenerator.makeIcon()),
-        onInfoWindowClick = { navController.navigate("trackDetailScreen/${record.trackRecordId }") },
-        onClick = {
-            it.showInfoWindow()
-            true
-        }
-    ) {
-        Box {
-            Canvas(
-                modifier = Modifier
-                    .width(105.dp)
-                    .height(50.dp)
-                    .align(Alignment.Center)
-            ) {
-                val trianglePath = Path().let {
-                    it.moveTo(this.size.width * .40f, this.size.height - 2f)
-                    it.lineTo(this.size.width * .50f, this.size.height + 30f)
-                    it.lineTo(this.size.width * .60f, this.size.height - 2f)
-                    it.close()
-                    it
-                }
-                drawRoundRect(
-                    GreyLightBlue,
-                    size = Size(this.size.width, this.size.height),
-                    cornerRadius = CornerRadius(60f)
-                )
-                drawPath(
-                    path = trianglePath,
-                    GreyLightBlue,
-                )
-            }
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-                modifier = Modifier
-                    .clip(RoundedCornerShape(25.dp))
-                    .padding(15.dp, 10.dp)
-                    .align(Alignment.Center)
-            ) {
-                Button(
-                    onClick = { }
-                ) {
-                    Text(stringResource(R.string.btn_details))
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun SensorStatusBar(
@@ -643,159 +565,7 @@ fun MarkerWindowPreview() {
     }
 }
 
-@Composable
-fun ComfortIndexRecordMapMarker(
-    record: ComfortIndexRecord
-) {
-    MarkerInfoWindow(
-        state = rememberMarkerState(
-            position = LatLng(record.latitude, record.longitude)
-        ),
-        icon = bitmapDescriptorFromVector(
-            LocalContext.current,
-            R.drawable.baseline_circle_12,
-            getColorFromCiRecord(record)
-        ),
-        onClick = { marker ->
-            marker.showInfoWindow()
-            true
-        }
-    ) {
-        Box {
-            Canvas(
-                modifier = Modifier
-                    .width(350.dp)
-                    .height(50.dp)
-                    .align(Alignment.Center)
-            ) {
-                val trianglePath = Path().let {
-                    it.moveTo(this.size.width * .40f, this.size.height - 2f)
-                    it.lineTo(this.size.width * .50f, this.size.height + 30f)
-                    it.lineTo(this.size.width * .60f, this.size.height - 2f)
-                    it.close()
-                    it
-                }
-                drawRoundRect(
-                    GreyLightBlue,
-                    size = Size(this.size.width, this.size.height),
-                    cornerRadius = CornerRadius(60f)
-                )
-                drawPath(
-                    path = trianglePath,
-                    GreyLightBlue,
-                )
-            }
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(2.dp),
-                modifier = Modifier
-                    .clip(RoundedCornerShape(25.dp))
-                    .padding(15.dp, 10.dp)
-                    .align(Alignment.Center)
-            ) {
-                Text(
-                    text = "${stringResource(R.string.ci_x)} ${record.comfortIndex}",
-                    color = Color.Black
-                )
-                Text(
-                    text = "${stringResource(R.string.speed_x)} ${record.bicycleSpeed}",
-                    color = Color.Black
-                )
-            }
-        }
-    }
-}
 
-@Composable
-fun UnoptimizedComfortIndexRecordMapMarker(
-    record: ComfortIndexRecord,
-) {
-    MarkerInfoWindow(
-        state = MarkerState(LatLng(record.latitude, record.longitude)),
-        icon = bitmapDescriptorFromVector(
-            LocalContext.current,
-            R.drawable.baseline_circle_12,
-            getColorFromCiRecord(record)
-        ),
-        onClick = { marker ->
-            marker.showInfoWindow()
-            true
-        }
-    ) {
-        Box {
-            Canvas(
-                modifier = Modifier
-                    .width(350.dp)
-                    .height(50.dp)
-                    .align(Alignment.Center)
-            ) {
-                val trianglePath = Path().let {
-                    it.moveTo(this.size.width * .40f, this.size.height - 2f)
-                    it.lineTo(this.size.width * .50f, this.size.height + 30f)
-                    it.lineTo(this.size.width * .60f, this.size.height - 2f)
-                    it.close()
-                    it
-                }
-                drawRoundRect(
-                    GreyLightBlue,
-                    size = Size(this.size.width, this.size.height),
-                    cornerRadius = CornerRadius(60f)
-                )
-                drawPath(
-                    path = trianglePath,
-                    GreyLightBlue,
-                )
-            }
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(2.dp),
-                modifier = Modifier
-                    .clip(RoundedCornerShape(25.dp))
-                    .padding(15.dp, 10.dp)
-                    .align(Alignment.Center)
-            ) {
-                Text(
-                    text = "${stringResource(R.string.ci_x)} ${record.comfortIndex}",
-                    color = Color.Black
-                )
-                Text(
-                    text = "${stringResource(R.string.speed_x)} ${record.bicycleSpeed}",
-                    color = Color.Black
-                )
-            }
-        }
-    }
-}
-
-fun getColorFromCiRecord(record: ComfortIndexRecord): Color {
-    val hue = 100 * record.comfortIndex
-    return Color.hsl(hue, 1f, 0.5f)
-}
-
-//https://towardsdev.com/jetpack-compose-custom-google-map-marker-erselan-khan-e6e04178a30b
-fun bitmapDescriptorFromVector(
-    context: Context,
-    vectorResId: Int,
-    color: Color
-): BitmapDescriptor? {
-    val drawable = ContextCompat.getDrawable(context, vectorResId) ?: return null
-    drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
-    drawable.colorFilter = BlendModeColorFilterCompat
-        .createBlendModeColorFilterCompat(color.toArgb(), BlendModeCompat.SRC_ATOP)
-    val bm = Bitmap.createBitmap(
-        drawable.intrinsicWidth,
-        drawable.intrinsicHeight,
-        Bitmap.Config.ARGB_8888
-    )
-
-    val canvas = android.graphics.Canvas(bm)
-    drawable.draw(canvas)
-    return BitmapDescriptorFactory.fromBitmap(bm)
-}
-
-fun mapFloatToHue(value: Float): Float {
-    return value * 130
-}
 
 @Composable
 //@Preview
---
+++ app/src/main/java/com/bk/bk1/compose/MapMarkers.kt
@@ -0,0 +1,226 @@
+package com.bk.bk1.compose
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Bitmap
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
+import androidx.navigation.NavController
+import com.bk.bk1.R
+import com.bk.bk1.models.ComfortIndexRecord
+import com.bk.bk1.ui.theme.GreyLightBlue
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.MarkerInfoWindow
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberMarkerState
+import com.google.maps.android.ui.IconGenerator
+
+@SuppressLint("SetTextI18n")
+@Composable
+fun TrackMarker(
+    record: ComfortIndexRecord,
+    navController: NavController,
+    markerView: View,
+    markerIconView: ImageView,
+    markerLabel: TextView,
+    iconGenerator: IconGenerator
+) {
+    markerIconView.setImageResource(R.drawable.bicycle_pin_48)
+    markerLabel.text = "${stringResource(R.string.label_track_x)} ${record.trackRecordId}"
+
+    iconGenerator.setBackground(null)
+    iconGenerator.setContentView(markerView)
+    MarkerInfoWindow(
+        state = rememberMarkerState(
+            position = LatLng(record.latitude, record.longitude)
+        ),
+        icon = BitmapDescriptorFactory.fromBitmap(iconGenerator.makeIcon()),
+        onInfoWindowClick = { navController.navigate("trackDetailScreen/${record.trackRecordId }") },
+        onClick = {
+            it.showInfoWindow()
+            true
+        }
+    ) {
+        Box {
+            Canvas(
+                modifier = Modifier
+                    .width(105.dp)
+                    .height(50.dp)
+                    .align(Alignment.Center)
+            ) {
+                val trianglePath = Path().let {
+                    it.moveTo(this.size.width * .40f, this.size.height - 2f)
+                    it.lineTo(this.size.width * .50f, this.size.height + 30f)
+                    it.lineTo(this.size.width * .60f, this.size.height - 2f)
+                    it.close()
+                    it
+                }
+                drawRoundRect(
+                    GreyLightBlue,
+                    size = Size(this.size.width, this.size.height),
+                    cornerRadius = CornerRadius(60f)
+                )
+                drawPath(
+                    path = trianglePath,
+                    GreyLightBlue,
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier
+                    .clip(RoundedCornerShape(25.dp))
+                    .padding(15.dp, 10.dp)
+                    .align(Alignment.Center)
+            ) {
+                Button(
+                    onClick = { }
+                ) {
+                    Text(stringResource(R.string.btn_details))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComfortIndexRecordMapMarker(
+    record: ComfortIndexRecord
+) {
+    MarkerInfoWindow(
+        state = rememberMarkerState(
+            position = LatLng(record.latitude, record.longitude)
+        ),
+        icon = bitmapDescriptorFromVector(
+            LocalContext.current,
+            R.drawable.baseline_circle_12,
+            getColorFromCiRecord(record)
+        ),
+        onClick = { marker ->
+            marker.showInfoWindow()
+            true
+        }
+    ) {
+        ComfortIndexRecordMapMarkerWindow(record)
+    }
+}
+
+@Composable
+fun UnoptimizedComfortIndexRecordMapMarker(
+    record: ComfortIndexRecord,
+) {
+    MarkerInfoWindow(
+        state = MarkerState(LatLng(record.latitude, record.longitude)),
+        icon = bitmapDescriptorFromVector(
+            LocalContext.current,
+            R.drawable.baseline_circle_12,
+            getColorFromCiRecord(record)
+        ),
+        onClick = { marker ->
+            marker.showInfoWindow()
+            true
+        }
+    ) {
+        ComfortIndexRecordMapMarkerWindow(record)
+    }
+}
+
+@Composable
+fun ComfortIndexRecordMapMarkerWindow(record: ComfortIndexRecord) {
+    Box {
+        Canvas(
+            modifier = Modifier
+                .width(350.dp)
+                .height(50.dp)
+                .align(Alignment.Center)
+        ) {
+            val trianglePath = Path().let {
+                it.moveTo(this.size.width * .40f, this.size.height - 2f)
+                it.lineTo(this.size.width * .50f, this.size.height + 30f)
+                it.lineTo(this.size.width * .60f, this.size.height - 2f)
+                it.close()
+                it
+            }
+            drawRoundRect(
+                GreyLightBlue,
+                size = Size(this.size.width, this.size.height),
+                cornerRadius = CornerRadius(60f)
+            )
+            drawPath(
+                path = trianglePath,
+                GreyLightBlue,
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(2.dp),
+            modifier = Modifier
+                .clip(RoundedCornerShape(25.dp))
+                .padding(15.dp, 10.dp)
+                .align(Alignment.Center)
+        ) {
+            Text(
+                text = "${stringResource(R.string.ci_x)} ${record.comfortIndex}",
+                color = Color.Black
+            )
+            Text(
+                text = "${stringResource(R.string.speed_x)} ${record.bicycleSpeed}",
+                color = Color.Black
+            )
+        }
+    }
+}
+
+fun getColorFromCiRecord(record: ComfortIndexRecord): Color {
+    val hue = 100 * record.comfortIndex
+    return Color.hsl(hue, 1f, 0.5f)
+}
+
+//https://towardsdev.com/jetpack-compose-custom-google-map-marker-erselan-khan-e6e04178a30b
+fun bitmapDescriptorFromVector(
+    context: Context,
+    vectorResId: Int,
+    color: Color
+): BitmapDescriptor? {
+    val drawable = ContextCompat.getDrawable(context, vectorResId) ?: return null
+    drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+    drawable.colorFilter = BlendModeColorFilterCompat
+        .createBlendModeColorFilterCompat(color.toArgb(), BlendModeCompat.SRC_ATOP)
+    val bm = Bitmap.createBitmap(
+        drawable.intrinsicWidth,
+        drawable.intrinsicHeight,
+        Bitmap.Config.ARGB_8888
+    )
+
+    val canvas = android.graphics.Canvas(bm)
+    drawable.draw(canvas)
+    return BitmapDescriptorFactory.fromBitmap(bm)
+}
